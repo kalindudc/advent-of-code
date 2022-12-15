474d35b601f6224b7c18bdb13d6c154d0cc10470
Kalindu De Costa <kalinduk.decosta@gmail.com>
2022-12-15 18:03:05 -0500

    Day 15

--- 2022/python/day-15/solve.py
+++ 2022/python/day-15/solve.py
@@ -1,5 +1,57 @@
 from pathlib import Path
 from aoc_io import Input
+from utils import Grid
+
+SENSOR = "S"
+BEACON = "B"
+
+def parse_lines(lines):
+  sensors = {}
+  distances = {}
+  for line in lines:
+    line = line.strip()
+    if len(line) == 0:
+      continue
+
+    parts = line.split(":")
+    sensor_descriptor = parts[0].strip()
+    beacon_descriptor = parts[1].strip()
+
+    raw_sensor_cords = sensor_descriptor[sensor_descriptor.index("x="):]
+    raw_beacon_cords = beacon_descriptor[beacon_descriptor.index("x="):]
+
+    sensor_cords_parts = raw_sensor_cords.split(",")
+    beacon_cords_parts = raw_beacon_cords.split(",")
+
+    sensor_cords = (int(sensor_cords_parts[1].split("=")[1].strip()), int(sensor_cords_parts[0].split("=")[1].strip()))
+    beacon_cords = (int(beacon_cords_parts[1].split("=")[1].strip()), int(beacon_cords_parts[0].split("=")[1].strip()))
+    sensors[sensor_cords] = beacon_cords
+    distances[sensor_cords] = distance(sensor_cords, beacon_cords)
+
+  print(sensors)
+  print(distances)
+
+  min_row = min([c[0] - distances[c] for c in sensors])
+  max_row = max([c[0] + distances[c] for c in sensors])
+  min_col = min([c[1] - distances[c] for c in sensors])
+  max_col = max([c[1] + distances[c] for c in sensors])
+
+  offset_row = min_row
+  offset_col = min_col
+  print(min_row, max_row, min_col, max_col)
+  print(offset_row, offset_col)
+  print((max_row - min_row) + 1, (max_col - min_col) + 1)
+
+  # grid = GridV2((max_row - min_row) + 1, (max_col - min_col) + 1, offset_row, offset_col)
+  # for sensor in sensors:
+  #   grid.set(sensor[0], sensor[1], SENSOR)
+  #   grid.set(sensors[sensor][0], sensors[sensor][1], BEACON)
+
+  return sensors, distances, min_row, max_row, min_col, max_col
+
+def distance(start, end):
+  return abs(end[0] - start[0]) + abs(end[1] - start[1])
+
 def main(args):
   if len(args) < 2:
     print("No mode given")
@@ -18,7 +70,31 @@ def main(args):
     print("No input given")
     return 1
 
+  sensors, distances, min_row, max_row, min_col, max_col = parse_lines(lines)
+  beacons = sensors.values()
+
+  row_to_inspect = 10 if len(args) < 3 else int(args[2])
+  print("Row to inspect: ", row_to_inspect)
+
+  no_beacon = 0
+  for col in range(min_col, max_col + 1):
+    current = (row_to_inspect, col)
+    if current in beacons:
+      continue
+
+    for sensor in sensors:
+      distance_to_sensor = distance(current, sensor)
+
+      if distance_to_sensor <= distances[sensor]:
+        no_beacon += 1
+        break
+
+  print("No beacon: ", no_beacon)
+
   return 0
+
+def tune_frequency(row, col):
+  return (col * 4000000) + row
 
 def main_2(args):
   if len(args) < 2:
@@ -38,4 +114,37 @@ def main_2(args):
     print("No input given")
     return 1
 
+  sensors, distances, min_row, max_row, min_col, max_col = parse_lines(lines)
+  beacons = sensors.values()
+
+  radius = 20 if len(args) < 4 else int(args[3])
+  steps = 0
+
+  for row in range(0, radius + 1):
+    col = -1
+    while col < radius + 1:
+      print(row, col)
+      steps += 1
+      col += 1
+      current = (row, col)
+      if current in beacons:
+        continue
+
+      found = True
+      for sensor in sensors:
+        distance_to_sensor = distance(current, sensor)
+
+        if distance_to_sensor <= distances[sensor]:
+          # skip the range of his sensor
+          col = sensor[1] + (distances[sensor] - abs(sensor[0] - current[0]))
+          found = False
+          break
+
+      if found:
+        print("Possible: ", current, tune_frequency(row, col))
+        print("Steps: ", steps)
+        return 0
+
+
+  # This is still super slow :(
   return 0
--- python/utils/grid.py
+++ python/utils/grid.py
@@ -1,7 +1,7 @@
 class Grid(object):
   def __init__(self, rows, cols, offset_row=0, offset_col=0, default_value="."):
-    self.rows = rows - offset_row
-    self.cols = cols - offset_col
+    self.rows = rows
+    self.cols = cols
     self.move_dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     self.map = [[default_value for i in range(self.cols)] for j in range(self.rows)]
     self.offset_row = offset_row
@@ -22,28 +22,26 @@ class Grid(object):
     return str(self.__str__())
 
   def __str__(self):
-    col_row_1 = "      "
-    col_row_2 = "      "
-    col_row_3 = "      "
+    fill = len(str(self.offset_row)) + 1
+    col_rows = [" " * (fill + 3) for i in range(fill)]
 
-    res = ""
+    res = "\n"
     for i in range(len(self.map[0])):
-      col_num = str(i + self.offset_col).zfill(3)
-      col_row_1 += col_num[0] + " "
-      col_row_2 += col_num[1] + " "
-      col_row_3 += col_num[2] + " "
+      col_num = str(i + self.offset_col).zfill(fill)
+      for j in range(len(col_rows)):
+        col_rows[j] += col_num[j] + " "
 
-    res += col_row_1 + "\n"
-    res += col_row_2 + "\n"
-    res += col_row_3 + "\n"
-    for i in range(len(self.map[0]) + 3):
+    for col_row in col_rows:
+      res += col_row + "\n"
+
+    for i in range(len(self.map[0]) + fill):
       res += "- "
     res += "\n"
 
     for i in range(len(self.map)):
       for j in range(len(self.map[0])):
         if j == 0:
-          res += str(i + self.offset_row).zfill(3) + " | "
+          res += str(i + self.offset_row).zfill(fill) + " | "
         res += str(self.map[i][j]) +  " "
       res += "\n"
     return res
@@ -51,11 +49,11 @@ class Grid(object):
   def is_edge(self, x, y):
     return x == 0 or y == 0 or x == self.rows - 1 or y == self.cols - 1
 
-  def get(self, x, y):
-    return self.map[x][y]
+  def get(self, row, col):
+    return self.map[row + (-1 * self.offset_row)][col + (-1 * self.offset_col)]
 
-  def set(self, x, y, value):
-    self.map[x][y] = value
+  def set(self, row, col, value):
+    self.map[row + (-1 * self.offset_row)][col + (-1 * self.offset_col)] = value
 
   def find_symbol(self, symbol):
     for i in range(len(self.map)):
@@ -91,19 +89,44 @@ class Grid(object):
         return path
 
       for i,j in self.move_dirs:
-        x = current[0] + i
-        y = current[1] + j
+        x = current[0] + (-1 * self.offset_row) + i
+        y = current[1]+ (-1 * self.offset_col) + j
 
         if x < 0 or y < 0 or x >= self.rows or y >= self.cols:
           continue
         if (x,y) in visited:
           continue
 
-        if can_visit(self, current, (x, y)):
+        real_x = x + self.offset_row
+        real_y = y + self.offset_col
+        if can_visit(self, current, (real_x, real_y)):
           visited.add((x,y))
-          queue.append(((x,y), path + [(x,y)]))
+          queue.append(((real_x,real_y), path + [(real_x,real_y)]))
 
     return []
 
+  def find_closest_value(self, start, value, can_visit=__can_visit):
+    queue = [start]
+    visited = set()
 
+    while len(queue) > 0:
+      current = queue.pop(0)
+      if self.get(current[0], current[1]) == value:
+        return current
 
+      for i,j in self.move_dirs:
+        x = current[0] + (-1 * self.offset_row) + i
+        y = current[1]+ (-1 * self.offset_col) + j
+
+        if x < 0 or y < 0 or x >= self.rows or y >= self.cols:
+          continue
+        if (x,y) in visited:
+          continue
+
+        real_x = x + self.offset_row
+        real_y = y + self.offset_col
+        if can_visit(self, current, (real_x, real_y)):
+          visited.add((x,y))
+          queue.append((real_x, real_y))
+
+    return ()
